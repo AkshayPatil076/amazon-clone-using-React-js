5e924f0ed9aa9a2b74ffe5acc7ec2fa3312ea4f1
AkshayPatil07695 <146464776+AkshayPatil076@users.noreply.github.com>
2024-04-20 20:06:58 +0530

    Update Navbar.js

--- src/page/Navbar.js
+++ src/page/Navbar.js
@@ -1,29 +1,7 @@
 import React from 'react'
 import './css/Navbar.css'
 import Logo from './img/logo.png'
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
-export default function Navbar() {
-  return (
-    <>
-        <nav className='navbar1'>
-                <div  className='Navleft' >
-                   <a href='' className='logo' > <img src={Logo} className='Logo' /> </a>
-                    <a href='' className='location1'><i class="bi bi-geo-alt"></i><div className='location2'>
-                           <p className='deliver'> Delivering to pathardi 414102</p> <b className='loup'>Update location</b>
-                        </div></a>
-                </div>
-                <div className='Navcenter'>
-                        <div className='sb'>
-                    <form >
-                        <select name="cars" id="cars" autofocus>
-                            <option value="All" ><b>All</b></option>
-                            <option value="saab">Saab</option>
-                            <option value="opel">Opel</option>
-                            <option value="audi">Audi</option>
-                        </select>import React from 'react'
-import './css/Navbar.css'
-import Logo from './img/logo.png'
+import Flg from './img/flg.jpg'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
@@ -101,43 +79,24 @@ export default function Navbar() {
                 </div>
                 <div className='Navright'>
                     <div className='lang'>
-                            <img  className='fla'/>
+                            <img src={Flg} className='fla'/>
                             <div className='eng'>En</div>
                     </div>
-                    <a className='sinup'>
+                    <a href="" className='sinup'>
                          <div className='accountlist'>
-                            Hello Sing in <br/>
-                            <b>Account & Lists</b>
+                            <div className='hello'>Hello Sing in</div> 
+                            <b className='account'>Account & Lists</b>
                          </div>
                         
                     </a>
 
-                </div>
-        </nav>
-        
-        <nav className='navbar2'>
-
-        </nav>
-    </>
-  )
-}
-
-                        <input type='text' className='searchbar' placeholder='Search Amazon.in'/>
-                        <button className='search'><i className='bi bi-search size'></i></button>
-                    </form>
-                        </div>
-                </div>
-                <div className='Navright'>
-                    <div className='lang'>
-                    
-                    </div>
-                    <a className='sinup'>
+                    <a href='' className='order'>
                          <div className='accountlist'>
-                            Hello Sing in <br/>
-                            <b>Account & Lists</b>
+                            <div className='hello'>Retums</div> 
+                            <b className='account'>& Orders</b>
                          </div>
+                        
                     </a>
-
                 </div>
         </nav>
         
